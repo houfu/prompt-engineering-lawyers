3e9cedfefe6107a47cf8a9da30133a2f27e8f5c1
houfu <houfu@users.noreply.github.com>
2024-07-27 09:20:21 +0800

    Fix: Models missing some fields

--- models.py
+++ models.py
@@ -6,15 +6,22 @@ from sqlmodel import SQLModel, Field, Relationship
 
 
 class User(SQLModel, table=True):
-    __tablename__ = 'users'
-    __table_args__ = {'schema': 'auth'}
+    __tablename__ = "users"
+    __table_args__ = {"schema": "auth"}
 
     id: UUID = Field(default=None, primary_key=True)
     username: str = Field()
     email: str = Field()
 
+
 class Profile(SQLModel, table=True):
-    id: UUID = Field(default=None, primary_key=True, foreign_key="auth.users.id", nullable=False, ondelete="CASCADE")
+    id: UUID = Field(
+        default=None,
+        primary_key=True,
+        foreign_key="auth.users.id",
+        nullable=False,
+        ondelete="CASCADE",
+    )
     name: Optional[str] = Field(default=None)
     email: str = Field()
     date_joined: datetime = Field(default=datetime.now())
@@ -24,14 +31,16 @@ class Profile(SQLModel, table=True):
 class Section(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field(default="New Section", description="Title of the Section")
-    section_order:int = 0
+    section_order: int = 0
     pages: list["Page"] = Relationship(back_populates="section")
 
 
 class Page(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field(default="New Page", description="Title of the page")
-    locked: bool = Field(default=False, description="Whether being a user is needed to view the page")
+    locked: bool = Field(
+        default=False, description="Whether being a user is needed to view the page"
+    )
     section: Section = Relationship(back_populates="pages")
     page_order: int = 0
 
@@ -40,9 +49,9 @@ class UserEvent(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     user: UUID = Field(default=None, foreign_key="profile.id")
     event: str = Field(description="What is happening. Should be eg. 'Complete page'")
-    page: Optional[int] = Field(default=None, description="If applicable, a page to which this applies to.",
-                                foreign_key="page.id")
+    page: Optional[int] = Field(
+        default=None,
+        description="If applicable, a page to which this applies to.",
+        foreign_key="page.id",
+    )
     time: datetime
-
-
-
