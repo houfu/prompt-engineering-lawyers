abfaff841d22eaa091d64ed64d7a251a0288111c
houfu <houfu@users.noreply.github.com>
2024-08-10 01:54:32 +0800

    Begin multipage app

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import streamlit as st
 
 from helpers import use_custom_css, write_footer
 
+st.set_page_config(layout="wide")
+
 if "api_success" not in st.session_state:
     st.session_state["api_success"] = False
 
 use_custom_css()
 
-pg = st.navigation([
-    st.Page("pages/Home.py")
-])
+pg = st.navigation({
+    "": [
+        st.Page("pages/Home.py", title="Home", default=True),
+    ],
+    "Getting Started": [
+        st.Page("pages/introduction.py", title="Introduction"),
+    ]
+}
+)
 pg.run()
 
 
