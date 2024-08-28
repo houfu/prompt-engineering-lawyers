9d7f1664cdc62d1f24e96b9acf2e0ae529a065cd
houfu <houfu@users.noreply.github.com>
2024-08-28 23:22:57 +0800

    Add chat_prompt

--- prompt_widget.py
+++ prompt_widget.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Optional
+from typing import Optional, Literal, TypedDict, List, Union
 
 import openai
 import streamlit as st
@@ -24,8 +24,6 @@ class SimplePromptHistoryItem(BaseModel):
     user: str
     assistant: Optional[str] = None
     date: datetime.datetime = datetime.datetime.now()
-
-
 
 
 def simple_prompt(title, **kwargs):
@@ -57,7 +55,7 @@ def simple_prompt(title, **kwargs):
             key=f"{content_key}-prompt",
             height=500 if long else None,
         )
-        submitted = st.form_submit_button("Submit")
+        submitted = st.form_submit_button("Submit", type="primary")
 
         if submitted:
             st.session_state[content_key].append(SimplePromptHistoryItem(user=prompt))
@@ -69,7 +67,9 @@ def simple_prompt(title, **kwargs):
                 st.session_state[history_key] - 1
             ]
             with st.chat_message("user"):
-                st.caption(f"On {current_history_item.date.strftime('%B %d, %Y at %I:%M%p')}")
+                st.caption(
+                    f"On {current_history_item.date.strftime('%B %d, %Y at %I:%M%p')}"
+                )
                 st.write(current_history_item.user)
 
             if current_history_item.assistant:
@@ -93,7 +93,9 @@ def simple_prompt(title, **kwargs):
                 ] = current_history_item
 
         def update_history_key():
-            st.session_state[history_key] = st.session_state[f"exercise-area-{title}-slider"]
+            st.session_state[history_key] = st.session_state[
+                f"exercise-area-{title}-slider"
+            ]
 
         if len(st.session_state[content_key]) > 1:
             st.slider(
@@ -106,3 +108,162 @@ def simple_prompt(title, **kwargs):
             )
 
     return exercise_container
+
+
+CHAT_PROMPT_ROLE = Literal["user", "assistant", "system"]
+
+
+class ChatPromptMessage(TypedDict):
+    role: CHAT_PROMPT_ROLE
+    content: str
+
+
+def chat_prompt(title: str, **kwargs):
+    history: List[ChatPromptMessage] = kwargs.get("history", [])
+    steps: Union[List[str]] = kwargs.get("steps", [])
+    long = kwargs.get("long", False)
+
+    if not isinstance(history, list) or not all(
+        isinstance(item, dict) and "role" in item and "content" in item
+        for item in history
+    ):
+        raise TypeError("history must be a list of ChatPromptMessage objects")
+
+    if not isinstance(steps, list) or not all(isinstance(item, str) for item in steps):
+        raise TypeError("steps must be a list of strings")
+
+    if check_openai_key():
+        return
+
+    # Initialize
+
+    content_key = f"exercise-area-{title}-content"
+    history_key = f"exercise-area-{title}-history"
+    steps_key = f"exercise-area-{title}-steps"
+
+    if content_key not in st.session_state:
+        st.session_state[content_key] = []
+        st.session_state[content_key].append(history)
+
+    if history_key not in st.session_state:
+        st.session_state[history_key] = 1
+
+    if steps_key not in st.session_state:
+        st.session_state[steps_key] = [0]
+
+    # Set messages to correct history
+
+    messages = st.session_state[content_key][st.session_state[history_key] - 1]
+
+    # Create exercise container
+    exercise_container = st.container(border=True)
+    exercise_container.header(f"Exercise: {title}")
+
+    # Produce conversation history in container
+    conversation = exercise_container.container()
+
+    with conversation:
+        for message in messages:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
+
+    # Create form area
+    def get_form_area(prompt=None):
+        with st.form(key=f"{content_key}-form", clear_on_submit=True):
+
+            step_area = st.empty()
+
+            prompt = st.text_area(
+                "Prompt",
+                prompt if prompt else "",
+                key=f"{content_key}-prompt",
+                height=500 if long else None,
+            )
+
+            if (
+                len(steps) > 0
+                and st.session_state[steps_key][st.session_state[history_key] - 1] == 0
+            ):
+                with step_area.container():
+                    st.caption(f"Prompt for this step:")
+                    st.chat_message("user").write(steps[0])
+
+            def update_step():
+                if len(steps) > 0:
+                    step_text = (
+                        steps[
+                            st.session_state[steps_key][
+                                st.session_state[history_key] - 1
+                            ]
+                        ]
+                        if st.session_state[steps_key][
+                            st.session_state[history_key] - 1
+                        ]
+                        < len(steps)
+                        else ""
+                    )
+                    if step_text:
+                        with step_area.container():
+                            st.caption(f"Prompt for this step:")
+                            st.chat_message("user").write(f"**{step_text}**")
+
+            submitted = st.form_submit_button("Submit", type="primary")
+
+            if submitted:
+                messages.append({"role": "user", "content": prompt})
+
+                if len(steps) > 0:
+                    st.session_state[steps_key][st.session_state[history_key] - 1] += 1
+
+                update_step()
+
+                client = openai.Client(api_key=st.session_state.openai_key)
+
+                with conversation:
+                    st.chat_message("user").write(prompt)
+                    with st.chat_message("assistant"):
+                        stream = client.chat.completions.create(
+                            model="gpt-4o-mini",
+                            messages=messages,
+                            stream=True,
+                        )
+                        response = st.write_stream(stream)
+
+                messages.append({"role": "assistant", "content": response})
+
+                st.session_state[content_key][
+                    st.session_state[history_key] - 1
+                ] = messages
+
+    form_area = exercise_container.empty()
+
+    with form_area:
+        get_form_area()
+
+    col1, col2 = exercise_container.columns([1, 5])
+    with col1:
+        reset = st.button("Reset")
+
+        if reset:
+            st.session_state[content_key].append(history)
+            st.session_state[history_key] = len(st.session_state[content_key])
+            st.session_state[steps_key].append(0)
+            st.rerun()
+
+    with col2:
+
+        def update_history_key():
+            st.session_state[history_key] = st.session_state[
+                f"exercise-area-{title}-slider"
+            ]
+            st.rerun()
+
+        if len(st.session_state[content_key]) > 1:
+            st.slider(
+                "History",
+                1,
+                len(st.session_state[content_key]),
+                st.session_state[history_key],
+                key=f"exercise-area-{title}-slider",
+                on_change=update_history_key,
+            )
